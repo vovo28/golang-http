8547f8c9c54a132e2150d0a85ee141a4c4ebe3fc
zhangzhiwei <181497765@qq.com>
2025-03-22 00:14:11 +0800

    feat: 新增http的json请求方法

--- http.go
+++ http.go
@@ -1,5 +1,13 @@
 package golang_http
 
+import (
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "io"
+  "net/http"
+)
+
 type Response[T any] struct {
   Status  string `json:"status"`
   Message string `json:"message"`
@@ -47,4 +55,33 @@ func (r *Response[T]) isSuccess() bool {
 
 func EmptyResponse[T any]() Response[T] {
   return Response[T]{Status: "", Message: "", Data: *new(T)}
+}
+
+// Post 发送POST请求 泛型参数和泛型响应
+func Post[T any, V any](url string, request Request[T]) (Response[V], error) {
+
+  // 将payload序列化为JSON
+  jsonData, err := json.Marshal(request)
+  if err != nil {
+    return EmptyResponse[V](), fmt.Errorf("failed to marshal payload: %w", err)
+  }
+
+  // 将JSON数据转换为bytes.Buffer
+  buffer := bytes.NewBuffer(jsonData)
+
+  resp, err := http.Post(url, "application/json", buffer)
+  if err != nil {
+    return EmptyResponse[V](), fmt.Errorf("failed to send request: %w", err)
+  }
+
+  body, err := io.ReadAll(resp.Body)
+
+  var response Response[V]
+  err = json.Unmarshal(body, &response)
+  if err != nil {
+    return EmptyResponse[V](), fmt.Errorf("failed to unmarshal response: %w", err)
+  }
+
+  defer resp.Body.Close()
+  return response, nil
 }
